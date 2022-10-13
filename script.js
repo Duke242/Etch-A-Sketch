1137e6a2bc2a4029c64414dbf6393757a7cdb95c
Duke <aboukhodr21@gmail.com>
2022-10-12 21:48:54 -0400

    random color added

--- script.js
+++ script.js
@@ -4,4 +4,16 @@ for (i = 0; i < 256; i++) {
     const div = document.createElement("div");
     container.appendChild(div);
     div.classList.add('new');  
-}
+    div.addEventListener('mouseenter', () => {
+        div.style.backgroundColor = randomColor();
+      });
+
+
+      function randomColor() {
+        rc = "#";
+        for (i = 0; i < 6; i++) {
+          rc += Math.floor(Math.random() * 16).toString(16);
+        }
+        return rc;
+      }
+    }
