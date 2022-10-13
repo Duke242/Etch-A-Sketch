80bc3ebc1bb18d1f037a069e859169261fa7219a
Duke <aboukhodr21@gmail.com>
2022-10-12 21:22:57 -0400

    grid is complete

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-const div = document.createElement("div")
+const container = document.querySelector('.container');
+
+for (i = 0; i < 256; i++) { 
+    const div = document.createElement("div");
+    container.appendChild(div);
+    div.classList.add('new');  
+}
