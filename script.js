401eb9d5159e1cb0a57b586301cb6e6bd6e9eab7
Duke <aboukhodr21@gmail.com>
2022-10-13 09:19:09 -0400

    finished project

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
 const container = document.querySelector('.container');
 
+let blackButtonPressed = false 
+let randomButtonPressed = false 
+let eraseButtonPressed = false 
+
+
 for (i = 0; i < 256; i++) { 
     const div = document.createElement("div");
     container.appendChild(div);
     div.classList.add('new');  
-    div.addEventListener('mouseenter', () => {
-        div.style.backgroundColor = randomColor();
+    div.addEventListener('mouseenter', () => {if (blackButtonPressed === true) {
+        div.style.backgroundColor = "black" 
+    } else if (eraseButtonPressed === true){ 
+        div.style.backgroundColor = "white"
+    } else if (randomButtonPressed === true) { div.style.backgroundColor = randomColor()}
+    else {return}
+    })} 
+        
+
+      const blackButton = document.querySelector('#blackButton')
+      const randomButton = document.querySelector('#randomButton')
+      const eraseButton = document.querySelector('#eraseButton')
+
+      blackButton.addEventListener('click', () => { 
+        blackButtonPressed = true 
+        randomButtonPressed = false 
+        eraseButtonPressed = false
+      });
+
+      eraseButton.addEventListener('click', () => { 
+        blackButtonPressed = false
+        randomButtonPressed = false 
+        eraseButtonPressed = true
+      });
+
+      randomButton.addEventListener('click', () => { 
+        blackButtonPressed = false
+        randomButtonPressed = true
+        eraseButtonPressed = false
       });
 
 
@@ -16,4 +48,6 @@ for (i = 0; i < 256; i++) {
         }
         return rc;
       }
-    }
+
+    
+    
